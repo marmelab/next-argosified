816c70fa4fffc7ae6236817b9a91caddb9ef5c1a
Alexis Janvier <contact@alexisjanvier.net>
2021-03-18 17:52:28 +0100

    sandbox

--- pages/blog.js
+++ pages/blog.js
@@ -1,27 +1,33 @@
+import Link from "next/link";
+
 function Blog({ characters }) {
-    return (
-        <>
-        <h2>Personnages</h2>
+  return (
+    <>
+      <h2>Personnages</h2>
       <ul>
         {characters.map((character) => (
-          <li>{character.name} ({character.id})</li>
+          <li>
+            <Link href={`/characters/${encodeURIComponent(character.id)}`}>
+                <a>{character.name}</a>
+            </Link>
+          </li>
         ))}
       </ul>
-      </>
-    )
-  }
+    </>
+  );
+}
 
-//   export async function getStaticProps() {
-  export async function getServerSideProps() {
-    // Call an external API endpoint to get posts
-    const res = await fetch('https://rickandmortyapi.com/api/character')
-    const data = await res.json()
-  
-    return {
-      props: {
-        characters: data.results,
-      },
-    }
-  }
-  
-  export default Blog
+  export async function getStaticProps() {
+// export async function getServerSideProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await res.json();
+
+  return {
+    props: {
+      characters: data.results,
+    },
+  };
+}
+
+export default Blog;
